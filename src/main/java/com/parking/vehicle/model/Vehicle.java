86a42465c5ecebd22b74a30e0eef93f6a481d296
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-03-01 11:16:25 -0300

    Criacao da entidade Vehicle

---
+++ src/main/java/com/parking/vehicle/model/Vehicle.java
@@ -0,0 +1,91 @@
+package com.parking.vehicle.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_vehicle")
+public class Vehicle implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Manufacturer manufacturer;
+    @NotNull
+    private String mark;
+    @ManyToOne
+    private Owner owner;
+    @ManyToOne
+    private ParkingLot parkingLot;
+
+
+    public Vehicle(){
+
+    }
+
+    public Vehicle(int manufacturer, String mark, Owner owner) {
+        this.manufacturer = Manufacturer.valueOf(manufacturer);
+        this.mark = mark;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vehicle)) return false;
+
+        Vehicle vehicle = (Vehicle) o;
+
+        return id != null ? id.equals(vehicle.id) : vehicle.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+
+}
